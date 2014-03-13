cbd5adec979ee2bab06cc17c2fcc1b0eabdc25d1
shivamkalra <shivamk.s.kalra@gmail.com>
2014-03-13 17:07:47 -0400

    urxvt size honor fix for awesome wm

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -366,6 +366,8 @@ awful.rules.rules = {
                     buttons = clientbuttons } },
    { rule = { class = "MPlayer" },
      properties = { floating = true } },
+   { rule = { class = "URxvt" },
+     properties = { size_hints_honor = false } },
    { rule = { class = "pinentry" },
      properties = { floating = true } },
    { rule = { class = "gimp" },
