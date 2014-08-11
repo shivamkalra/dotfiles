5684d7c4cafbb883f67d9a7282c5158d6d425cd5
shivamkalra <shivamk.s.kalra@gmail.com>
2014-08-11 03:05:23 -0400

    Addign ido recentf

--- emacs/sk-utils.el
+++ emacs/sk-utils.el
@@ -15,4 +15,11 @@
   (interactive)
   (byte-recompile-directory user-emacs-directory 0))
 
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+
 (provide 'sk-utils)
