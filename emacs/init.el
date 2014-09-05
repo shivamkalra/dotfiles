44a2042e2d4ce8c2e869619eafdab25d6035ef89
Shivam Kalra <skalra@klick.com>
2014-09-04 20:35:01 -0400

    Modifying init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,21 @@
 ;;; init.el --- The Emacs Initialization File
 (setq message-log-max 16384)
 
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+
+(setq gc-cons-threshold 20000000)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Allow font-lock-mode to do background parsing
+(setq jit-lock-stealth-time 1
+      jit-lock-stealth-load 100
+      jit-lock-chunk-size 1000
+      jit-lock-defer-time 0.01)
+
 (defun emacs-d (filename)
   "Expand FILENAME relative to `user-emacs-directory'."
   (expand-file-name filename user-emacs-directory))
@@ -45,6 +60,9 @@
 ;; Visible bell..geez
 (setq visible-bell t)
 
+;; disable the case matching for file name
+(setq read-file-name-completion-ignore-case t)
+
 ;; set the *sick* fonts for gui
 (add-to-list 'default-frame-alist
 	     '(font . "Fantasque Sans Mono-10:weight=black"))
@@ -61,3 +79,15 @@
 
 (global-set-key (kbd "C-c y s") 'sk-youtube-search)
 (global-set-key (kbd "C-c y p") 'sk-youtube-play-pause)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doc-view-continuous t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
