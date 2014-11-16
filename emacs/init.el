7f27d9e0737869092a519608766361406e6d8479
Shivam Kalra <skalra@klick.com>
2014-11-16 04:34:25 -0500

    Adding git gutter

--- emacs/init.el
+++ emacs/init.el
@@ -326,6 +326,24 @@
     (elpy-enable)
     (elpy-use-ipython)))
 
+;; git
+(use-package git-gutter
+  :init
+  (progn
+    (global-git-gutter-mode t)
+    (git-gutter:linum-setup))
+  :config
+  (progn
+    (global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+    (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+    ;; Jump to next/previous hunk
+    (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+    (global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+    ;; Stage current hunk
+    (global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+    ;; Revert current hunk
+    (global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)))
+
 ;; popwin
 (use-package popwin
   :config
